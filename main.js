97d4e675c1d98aed0f0ffea466b0e7225e866fb3
Joyce Kwak <joycekwak@Joyces-MacBook-Air.local>
2023-02-08 22:31:39 -0800

    done

--- main.js
+++ main.js
@@ -1,4 +1,104 @@
-hello world
-hackathon
 
-this is a test 
+//make a clock
+
+const showTime = () => {
+  let time = new Date();
+  const month = time.getMonth() + 1;
+  const day = time.getDate();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  let am_pm = 'AM';
+
+  if (hour === 0) {
+    hour = 12;
+  }
+
+  if (hour > 12) {
+    hour -= 12;
+    am_pm = 'PM'
+  }
+  hour = (hour < 10) ? '0' + hour : hour;
+  min = (min < 10) ? '0' + min : min;
+  sec = (sec < 10) ? '0' + sec : sec;
+
+  const currentTime = `${hour}:${min}:${sec}  ${am_pm}`
+
+  document.getElementById('clock-display').innerText = currentTime;
+
+  setTimeout(showTime, 1000);
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  showTime();
+
+  async function getWeather() {
+    const fetchWeather = await fetch('https://api.weather.gov/gridpoints/MTR/91,89/forecast');
+    const data = await fetchWeather.json();
+    const weatherBox = document.getElementById('temperature')
+    weatherBox.innerHTML = `${data.properties.periods[0].temperature}°F`
+  }
+
+
+  getWeather();
+
+
+  const submitButton = document.querySelector('#submit');
+
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const userTaskContainer = document.createElement('div')
+    userTaskContainer.id = 'user-task-container'
+    
+    // create a check box
+    const checkBox = document.createElement('button')
+    checkBox.id = 'check-box'
+
+    checkBox.addEventListener('click', (e) => {
+        // e.preventDefault();
+        checkBox.style.backgroundColor = 'black';
+        
+    })
+
+    userTaskContainer.appendChild(checkBox);
+    const taskWords = document.createElement('span');
+    userTaskContainer.appendChild(taskWords)
+    // user adds tasks to to do list 
+    const usersTask = document.getElementById('task');
+    const task = document.createElement('li')
+    
+    task.id = 'task-text'
+    // if (usersTask.value.length > )
+    taskWords.innerHTML = usersTask.value;
+    // task.value.id = 'users-tasks'
+    usersTask.appendChild(userTaskContainer)
+    
+    // create a trash can (removes elements)
+    let trashCanImage = document.createElement('img');
+    trashCanImage.src = 'trash-can-icon.png';
+    const deleteButton = trashCanImage;
+    deleteButton.id = 'trash-can';
+    
+    deleteButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      deleteButton.parentNode.remove();
+      
+    })
+    userTaskContainer.appendChild(trashCanImage);
+    
+    
+    document.querySelector('ul').appendChild(userTaskContainer);
+    
+    // empty user input 
+    usersTask.value = ''; 
+  })
+
+  document.body.style.background = "#"+((1<<24)*Math.random()|0).toString(16);
+
+
+})
